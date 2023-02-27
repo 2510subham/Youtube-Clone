c219d00299401af59d1cd3158a1bac6d3b042baa
Subham Agarwal <subhamagarwal400@gmail.com>
2023-02-27 21:38:07 +0530

    youtube clone done

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,29 +1,38 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
-import {useState,useEffect} from 'react';
-import {Box} from '@mui/material'
-import {Videos,ChannelCard} from '../components';
+import { useState, useEffect } from 'react';
+import { Box } from '@mui/material'
+import { Videos, ChannelCard } from './index';
 import { fetchFromApi } from '../utils/fetchFromApi';
-
-
-
 
 const ChannelDetail = () => {
   const [channelDetail, setchannelDetail] = useState(null)
-  const [videos,setVideos]=useState([])
+  const [videos, setVideos] = useState([])
   // console.log(channelDetail)
-  const {id}=useParams();
+  const { id } = useParams();
   useEffect(() => {
     fetchFromApi(`channels?part=snippet&id=${id}`)
-    .then((data)=>setchannelDetail(data?.items[0]));
+      .then((data) => setchannelDetail(data?.items[0]));
 
     fetchFromApi(`search?channelId=${id}&part=snippet&order=date`)
-    .then((data)=>setVideos(data?.items));
-  },[id])
+      .then((data) => setVideos(data?.items));
+  }, [id])
   return (
-   <Box>
-    {id}
-   </Box>
+    <Box minHeight='95vh'>
+      <Box>
+        <div style={{
+          background: 'linear-gradient(90deg,rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+          height: '300px', zIndex: 10
+
+        }} />
+        <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
+
+      </Box>
+      <Box display="flex" p="2" >
+        <Box sx={{ mr: { sm: '180px' } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
   )
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,9 +4,27 @@ import {useNavigate} from 'react-router-dom'
 import {Paper,IconButton} from '@mui/material'
 import {Search} from '@mui/icons-material'
 
+const SearchBar = () =>
+{
+    const [searchTerm, setsearchTerm] = useState("")
+    const navigate=useNavigate();
 
-const SearchBar = () => (
-        <Paper component="form" onSubmit={()=>{}} 
+    function searching(e)
+    {
+        setsearchTerm(e.target.value)
+    }
+    function handleSubmit(e)
+    {
+        e.preventDefault();
+        if(searchTerm)
+        {
+            navigate(`/search/${searchTerm}`)
+            setsearchTerm("")
+
+        }
+    }
+    return (
+        <Paper component="form" onSubmit={handleSubmit} 
         sx={{
             borderRadius:20,
             border:'1px solid #e3e3e3',
@@ -14,13 +32,13 @@ const SearchBar = () => (
             boxShadow:'none',
             mr:{sm:5}
         }} >
-            <input className="search-bar" placeholder="search..." value="" onChange={()=>{}}  
+            <input className="search-bar" placeholder="search..." value={searchTerm} onChange={searching}
              />
              <IconButton type="submit" sx={{p:'10px' ,color:'red'}}>
                 <Search/>
              </IconButton>
         </Paper>
-)
-
+    )
+}
 
 export default SearchBar
